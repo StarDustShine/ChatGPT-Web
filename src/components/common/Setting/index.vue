49bb4a317c7e4d73ac27913711881d7850048afe
StarDustShine <108602877+StarDustShine@users.noreply.github.com>
2023-03-06 21:37:31 +0800

    Update index.vue

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -1,1 +1,42 @@
+<script setup lang='ts'>
+import { computed, ref } from 'vue'
+import { NCard, NModal, NTabPane, NTabs } from 'naive-ui'
+import General from './General.vue'
+import About from './About.vue'
+import { SvgIcon } from '@/components/common'
 
+const props = defineProps<Props>()
+
+const emit = defineEmits<Emit>()
+
+interface Props {
+  visible: boolean
+}
+
+interface Emit {
+  (e: 'update:visible', visible: boolean): void
+}
+
+const active = ref('General')
+
+const reload = ref(false)
+
+const show = computed({
+  get() {
+    return props.visible
+  },
+  set(visible: boolean) {
+    emit('update:visible', visible)
+  },
+})
+
+function handleReload() {
+  reload.value = true
+  setTimeout(() => {
+    reload.value = false
+  }, 0)
+}
+</script>
+
+<template>
+</template>
